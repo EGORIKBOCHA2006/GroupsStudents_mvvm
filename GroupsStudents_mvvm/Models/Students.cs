a7ac5e3ab3bd04f52b81f54af457bb021f5257c9
Егор <@DESKTOP-LBTERG7>
2025-03-22 17:57:57 +0300

    Отображение даты рождения в DatePicker

--- GroupsStudents_mvvm/Models/Students.cs
+++ GroupsStudents_mvvm/Models/Students.cs
@@ -13,10 +13,12 @@ namespace GroupsStudents_mvvm.Models
         public string Patronymic { get; set; }
         public DateTime Birthday { get; set; }
         public int Rating { get; set; }
+        public string Description { get; set; }
     }
     internal class Group
     {
         public string Name { get; set; }
         public ICollection<Student> Students { get; set; }
+        public string Descriptioin { get; set; }
     }
 }
