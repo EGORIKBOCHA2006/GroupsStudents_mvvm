685cdd221835d6b929b3581fb591d78d5eea93bd
Егор <@DESKTOP-LBTERG7>
2025-03-22 16:20:23 +0300

    Модель студентов и групп

---
+++ GroupsStudents_mvvm/Models/Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupsStudents_mvvm.Models
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Rating { get; set; }
+    }
+    internal class Group
+    {
+        public string Name { get; set; }
+        public ICollection<Student> Students { get; set; }
+}
---
+++ GroupsStudents_mvvm/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GroupsStudents_mvvm.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupsStudents_mvvm.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+    }
+}
